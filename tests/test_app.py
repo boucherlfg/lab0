cf99908be8e299ec1e16cdb5ebbf6671070df05c
Felix-Gabriel Boucher-Luneau <boucherlfg@gmail.com>
2025-05-23 15:04:41 -0400

    update tests more digeste

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,28 +1,18 @@
 # pylint: skip-file
 
 import pytest
-from unittest.mock import patch
 from src import app
+from unittest.mock import patch
 
 @pytest.fixture
 def mock_print():
-    with patch('builtins.print') as mock:
+    with patch("builtins.print") as mock:
         yield mock
 
 def test_func(mock_print):
-    class Test:
-        check : str = ""
-    def assign_check(value):
-        Test.check = value
-    mock_print.side_effect = assign_check
     app.func("hello")
-    assert Test.check == "hello"
+    mock_print.assert_called_with("hello")
 
-def test_func_not_toto(mock_print):
-    class Test:
-        check : str = ""
-    def assign_check(value):
-        Test.check = value
-    mock_print.side_effect = assign_check
-    app.func("hello")
-    mock_print.assert_called()
+def test_func_error():
+    with pytest.raises(Exception):
+        app.func("test" + 1)
